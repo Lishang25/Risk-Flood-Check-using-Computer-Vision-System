107ab05950eac376bb083b22626b90900de95d97
Lishang Gajendra <lishangajendra2005@gmail.com>
2026-08-31 14:14:16 +0530

    Update FLOOD MONITORING.py

--- FLOOD MONITORING.py
+++ FLOOD MONITORING.py
@@ -9,7 +9,7 @@
 
 import subprocess, sys, os, time, importlib
 
-OUTPUT_FILE = "output_flood_monitoring_ai.mp3"
+OUTPUT_FILE = "output_flood_monitoring_ai.mp4"
 
 PACKAGES = [
     "ultralytics", "opencv-python", "numpy", "torch", "torchvision",
